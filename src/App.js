a9aeb207c613e5a81c26319ba065a96712368da1
Infinite88 <jlew2024@yahoo.com>
2018-04-16 15:20:01 -0400

    fixed error in image counter

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
           })
             .then(response => response.json())
             .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
+              this.setState(Object.assign(this.state.user, { entries: counts}))
             })
               .catch(err => console.log(err));
         }
